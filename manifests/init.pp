adf402a8f3ccfafd921b25946c9e53dcd5dd7189
Martin Dluhos <martin@gnu.org>
2012-04-02 23:59:27 -0500

    Generated module skeleton using puppet-module generate.

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: mediawiki
+#
+# This module manages mediawiki
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class mediawiki {
+
+
+}
