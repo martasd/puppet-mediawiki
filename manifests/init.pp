da2a90d6bfd98179ff4b920509fe1a3761a9fa84
Martin Dluhos <martin@gnu.org>
2012-04-16 15:14:43 -0500

    Started a new branch in which the module does not depend on any other modules.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,16 @@
 # Class: mediawiki
 #
-# This module manages mediawiki.
+# This module manages a multi-tenant mediawiki installation.
 #
 # Parameters:
 #
 # [*package_ensure*]
-# [*admin*]
-# [*servername*]
-# [*serveralias*]
 # [*db_name*]
 # [*db_user*]
 # [*db_password*]
 # [*memory*]
+#
+# To be implemented:
 # [*ip*]
 # [*port*]
 #
@@ -19,78 +18,62 @@
 #
 # Requires:
 #
+# None
+#
 # Sample Usage:
 #
 class mediawiki (
-    $package_ensure = 'latest',
-    $admin,
-    $servername = $name,
-    $serveralias = $name,
-    $db_name = $name,
-    $db_user = 'mediawiki',
-    $db_password = 'mediawiki',
-    $memory = 2048,
-    $ip,
-    $port = 80) {
+  $package_ensure = 'latest',
+  $db_name,
+  $db_user,
+  $db_password = 'my_password',
+  $memory = 2048,
+  ) {
 
   package { 'mediawiki':
     name => $mediawiki::params::package_name,
     ensure => $package_ensure,
   }
 
-  # ghoneycutt's apache module needs to be extended and improved
-  class { 'apache:php': }
+  class { 'mediawiki::apache': }
 
-  class { 'mysql::server': }
-
-  # Create a MySQL database
-  mysql::db  { $db_name:
-    user => $db_user,
-         password => $db_password,
-         host => $::hostname
-  }
-
-
-  class { 'memcached': 
-    max_memory => $memory
-  }
-
-  class { 'authconfig': }
-
+  class { 'mediawiki::php': }
+  
+  class { 'mediawiki::mysql_server': }
 
   file { 'wikis':
-            path    => '${mediawiki_root}/wikis',
-            owner   => 'root',
-            group   => 'root',
-            ensure  => directory,
-            mode    => 0755,
-            require => Package['mediawiki'];
+    path    => '${mediawiki_root}/wikis',
+    owner   => 'root',
+    group   => 'root',
+    ensure  => directory,
+    mode    => 0755,
+    require => Package['mediawiki'];
   }
 
   file { 'mywiki':
-            path    => '${mediawiki_root}/wikis/${name}',
-            owner   => 'root',
-            group   => 'root',
-            ensure  => directory,
-            mode    => 0755,
-            require => File['wikis'];
+    path    => '${mediawiki_root}/wikis/${name}',
+    owner   => 'root',
+    group   => 'root',
+    ensure  => directory,
+    mode    => 0755,
+    require => File['wikis'];
   }
 
   file { 'skins':
-            path    => '${mediawiki_root}/wikis/${name}/skins',
-            owner   => 'www-data',
-            group   => 'www-data',
-            ensure  => directory,
-            mode    => 0755,
-            require => File['mywiki'];
+    path    => '${mediawiki_root}/wikis/${name}/skins',
+    owner   => 'www-data',
+    group   => 'www-data',
+    ensure  => directory,
+    mode    => 0755,
+    require => File['mywiki'];
   }
 
   file { 'config_file':
-            path    => '${mediawiki_root}/wikis/${name}/index.php',
-            owner   => 'www-data',
-            group   => 'www-data',
-            content => template('index.php.erb'),
-            mode    => 0755;
+    path    => '${mediawiki_root}/wikis/${name}/index.php',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => template('index.php.erb'),
+    mode    => 0755;
   }
 
   # Add other files and directories
