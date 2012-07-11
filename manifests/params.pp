c61cb34eebe12d3f50c99c13a443c1de62500439
Martin Dluhos <martin@gnu.org>
2012-07-11 14:21:15 -0700

    Updated params.pp to include variables for Debian Wheezy.

--- manifests/params.pp
+++ manifests/params.pp
@@ -25,6 +25,17 @@ class mediawiki::params {
     redhat: {
     }
     debian: {
+      $packages           = ['php5', 'php5-mysql',
+                             'mediawiki', 'mediawiki-extensions']
+      $conf_dir           = '/etc/mediawiki'
+      $instance_root_dir  = '/var/www'
+      $apache_daemon      = '/usr/sbin/apache2'
+      $installation_files = ['api.php', 'config', 'extensions','img_auth.php',
+                             'includes', 'index.php', 'load.php', 'languages',
+                             'maintenance', 'mw-config', 'opensearch_desc.php',
+                             'profileinfo.php', 'redirect.php', 'redirect.phtml',
+                             'resources', 'skins', 'thumb_handler.php',
+                             'thumb.php', 'wiki.phtml']
     }
     ubuntu: {
       $packages           = ['php5', 'php5-mysql',
