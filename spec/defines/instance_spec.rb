2f281867b6c0b4d5938e41a5faadfe52cc5368e8
Martin Dluhos <martin@gnu.org>
2012-07-13 17:54:45 -0700

    Updated an rspec test for instance.pp.

--- spec/defines/instance_spec.rb
+++ spec/defines/instance_spec.rb
@@ -24,7 +24,7 @@ describe 'mediawiki::instance', :type => :define do
     end
 
     let(:title) do
-      "dummy_instance"
+      'dummy_instance'
     end
     
     it {
@@ -59,14 +59,21 @@ describe 'mediawiki::instance', :type => :define do
         'mode'     => '0664',
       )
       
-      #should contain_mediawiki__files(["api.php", "config", "extensions", 
-      #                                 "img_auth.php", "includes", "index.php",
-      #                                 "load.php", "languages", "maintenance", 
-      #                                 "mw-config", "opensearch_desc.php", 
-      #                                 "profileinfo.php", "redirect.php", 
-      #                                 "redirect.phtml", "resources", "skins",
-      #                                 "thumb_handler.php", "thumb.php", 
-      #                                 "wiki.phtml"])
+      ['api.php', 'config', 'extensions','img_auth.php',
+       'includes', 'index.php', 'load.php', 'languages',
+       'maintenance', 'mw-config', 'opensearch_desc.php',
+       'profileinfo.php', 'redirect.php', 'redirect.phtml',
+       'resources', 'skins', 'thumb_handler.php',
+       'thumb.php', 'wiki.phtml'].each do |f| 
+      should contain_file(f).with(
+        'ensure'  => 'link',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0755',
+        'target'  => "/etc/mediawiki/dummy_instance/#{f}",
+                                 )
+      end
+
       
       should contain_file('wiki_instance_dir_link').with(
         'ensure'   => 'link',
