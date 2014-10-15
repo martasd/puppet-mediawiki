0a464f144e573af469194d05da6b7623ec0d1371
David Schmitt <david@dasz.at>
2014-10-15 17:07:09 +0200

    Remove and document dependency to installed mysql-server

--- spec/classes/mediawiki_spec.rb
+++ spec/classes/mediawiki_spec.rb
@@ -28,7 +28,6 @@ describe 'mediawiki', :type => :class do
       should contain_class('mediawiki::params')
       should contain_package('php5').with('ensure' => 'latest')
       should contain_package('php5-mysql').with('ensure'=> 'latest')
-      should contain_class('mysql::server').with('config_hash' => {'root_password' => 'long_password'})
       should contain_class('memcached').with('max_memory' => '2048')
       should contain_file('mediawiki_conf_dir').with(
         'ensure'  => 'directory',
@@ -69,7 +68,6 @@ describe 'mediawiki', :type => :class do
       should contain_class('mediawiki::params')
       should contain_package('php5').with('ensure' => 'installed')
       should contain_package('php5-mysql').with('ensure' => 'installed')
-      should contain_class('mysql::server').with('config_hash' => {'root_password' => 'long_password'})
       should contain_class('memcached').with('max_memory' => '1024')
       should contain_file('mediawiki_conf_dir').with(
         'ensure'  => 'directory',
