f5745405e06d45994db307926fabd91c41f88619
David Schmitt <david@dasz.at>
2014-10-16 09:55:01 +0200

    Add missing comma

--- tests/instance.pp
+++ tests/instance.pp
@@ -1,7 +1,7 @@
 mediawiki::instance { 'my_wiki1':
   db_password => 'super_long_password',
   db_name     => 'wiki1',
-  db_user     => 'wiki1_user'
+  db_user     => 'wiki1_user',
   port        => '80',
   status      => 'present'
 }
